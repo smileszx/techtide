abb6f80e2d24408a11af924ae00dd54d7bb985ba
smile <smile.szx@outlook.com>
2019-09-05 23:40:46 +0800

    add kafka

---
+++ techtide-service/src/main/java/com/ai/tide/rpc/kafka/KafkaClient.java
@@ -0,0 +1,15 @@
+package com.ai.tide.rpc.kafka;
+
+import kafka.Kafka;
+
+/**
+ * @Description TODO
+ * @Author victor su
+ * @Date 2019/9/5 23:37
+ **/
+public class KafkaClient {
+
+    public static void main(String[] args) {
+        Kafka kafka = new Kafka();
+    }
+}
